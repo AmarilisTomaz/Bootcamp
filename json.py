c00ba86a903c8d532735cea6dd9562d7f5d03b00
AmarilisTomaz <amarilisxd@gmail.com>
2023-07-13 00:29:05 -0300

    confg ambiente

---
+++ json.py
@@ -0,0 +1,401 @@
+variavel = {
+"data": {
+    "best_day": {
+      "created_at": "2023-06-15T03:10:27Z",
+      "date": "2023-06-25",
+      "id": "568d7326-643c-46f1-a14e-995c58fb19fe",
+      "modified_at": "2023-06-27T03:10:17Z",
+      "text": "9 hrs 13 mins",
+      "total_seconds": 33216.915627
+    },
+    "categories": [
+      {
+        "decimal": "45.40",
+        "digital": "45:24",
+        "hours": 45,
+        "minutes": 24,
+        "name": "Coding",
+        "percent": 100.0,
+        "text": "45 hrs 24 mins",
+        "total_seconds": 163461.125813
+      }
+    ],
+    "created_at": "2023-06-15T03:10:26Z",
+    "daily_average": 23352.0,
+    "daily_average_including_other_language": 23352.0,
+    "days_including_holidays": 7,
+    "days_minus_holidays": 7,
+    "dependencies": [
+      {
+        "decimal": "5.83",
+        "digital": "5:50",
+        "hours": 5,
+        "minutes": 50,
+        "name": "flask",
+        "percent": 30.0,
+        "text": "5 hrs 50 mins",
+        "total_seconds": 21002.893283
+      },
+      {
+        "decimal": "4.63",
+        "digital": "4:38",
+        "hours": 4,
+        "minutes": 38,
+        "name": "requests",
+        "percent": 23.83,
+        "text": "4 hrs 38 mins",
+        "total_seconds": 16683.572674
+      },
+      {
+        "decimal": "2.37",
+        "digital": "2:22",
+        "hours": 2,
+        "minutes": 22,
+        "name": "function",
+        "percent": 12.25,
+        "text": "2 hrs 22 mins",
+        "total_seconds": 8575.613
+      },
+      {
+        "decimal": "2.37",
+        "digital": "2:22",
+        "hours": 2,
+        "minutes": 22,
+        "name": "flask_socketio",
+        "percent": 12.19,
+        "text": "2 hrs 22 mins",
+        "total_seconds": 8534.553739
+      },
+      {
+        "decimal": "0.55",
+        "digital": "0:33",
+        "hours": 0,
+        "minutes": 33,
+        "name": "\"../static/script/game.js\"",
+        "percent": 2.89,
+        "text": "33 mins",
+        "total_seconds": 2020.529442
+      },
+      {
+        "decimal": "0.53",
+        "digital": "0:32",
+        "hours": 0,
+        "minutes": 32,
+        "name": "shared",
+        "percent": 2.82,
+        "text": "32 mins",
+        "total_seconds": 1971.213
+      },
+      {
+        "decimal": "0.53",
+        "digital": "0:32",
+        "hours": 0,
+        "minutes": 32,
+        "name": "yaml",
+        "percent": 2.82,
+        "text": "32 mins",
+        "total_seconds": 1971.213
+      },
+      {
+        "decimal": "0.53",
+        "digital": "0:32",
+        "hours": 0,
+        "minutes": 32,
+        "name": "csv",
+        "percent": 2.82,
+        "text": "32 mins",
+        "total_seconds": 1971.213
+      },
+      {
+        "decimal": "0.48",
+        "digital": "0:29",
+        "hours": 0,
+        "minutes": 29,
+        "name": "threading",
+        "percent": 2.5,
+        "text": "29 mins",
+        "total_seconds": 1746.801
+      },
+      {
+        "decimal": "0.45",
+        "digital": "0:27",
+        "hours": 0,
+        "minutes": 27,
+        "name": "socket",
+        "percent": 2.35,
+        "text": "27 mins",
+        "total_seconds": 1647.635
+      },
+      {
+        "decimal": "0.12",
+        "digital": "0:07",
+        "hours": 0,
+        "minutes": 7,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js\"",
+        "percent": 0.68,
+        "text": "7 mins",
+        "total_seconds": 474.552
+      },
+      {
+        "decimal": "0.12",
+        "digital": "0:07",
+        "hours": 0,
+        "minutes": 7,
+        "name": "\"/socket.io/socket.io.js\"",
+        "percent": 0.64,
+        "text": "7 mins",
+        "total_seconds": 445.635598
+      },
+      {
+        "decimal": "0.10",
+        "digital": "0:06",
+        "hours": 0,
+        "minutes": 6,
+        "name": "json",
+        "percent": 0.57,
+        "text": "6 mins",
+        "total_seconds": 398.969
+      },
+      {
+        "decimal": "0.10",
+        "digital": "0:06",
+        "hours": 0,
+        "minutes": 6,
+        "name": "\"\"",
+        "percent": 0.55,
+        "text": "6 mins",
+        "total_seconds": 385.585
+      },
+      {
+        "decimal": "0.10",
+        "digital": "0:06",
+        "hours": 0,
+        "minutes": 6,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.4.1/socket.io.js\"",
+        "percent": 0.53,
+        "text": "6 mins",
+        "total_seconds": 373.764
+      },
+      {
+        "decimal": "0.10",
+        "digital": "0:06",
+        "hours": 0,
+        "minutes": 6,
+        "name": "\"https://code.jquery.com/jquery-3.6.0.min.js\"",
+        "percent": 0.53,
+        "text": "6 mins",
+        "total_seconds": 373.764
+      },
+      {
+        "decimal": "0.08",
+        "digital": "0:05",
+        "hours": 0,
+        "minutes": 5,
+        "name": "random",
+        "percent": 0.45,
+        "text": "5 mins",
+        "total_seconds": 314.963
+      },
+      {
+        "decimal": "0.07",
+        "digital": "0:04",
+        "hours": 0,
+        "minutes": 4,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.2/socket.io.js\"",
+        "percent": 0.37,
+        "text": "4 mins",
+        "total_seconds": 261.276
+      },
+      {
+        "decimal": "0.05",
+        "digital": "0:03",
+        "hours": 0,
+        "minutes": 3,
+        "name": "deckofcards",
+        "percent": 0.29,
+        "text": "3 mins",
+        "total_seconds": 203.272
+      },
+      {
+        "decimal": "0.03",
+        "digital": "0:02",
+        "hours": 0,
+        "minutes": 2,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.1.2/socket.io.js\"",
+        "percent": 0.25,
+        "text": "2 mins",
+        "total_seconds": 172.732
+      },
+      {
+        "decimal": "0.03",
+        "digital": "0:02",
+        "hours": 0,
+        "minutes": 2,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.1/socket.io.js\"",
+        "percent": 0.21,
+        "text": "2 mins",
+        "total_seconds": 147.503
+      },
+      {
+        "decimal": "0.02",
+        "digital": "0:01",
+        "hours": 0,
+        "minutes": 1,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.1/socket.io.js\"",
+        "percent": 0.16,
+        "text": "1 min",
+        "total_seconds": 110.076
+      },
+      {
+        "decimal": "0.02",
+        "digital": "0:01",
+        "hours": 0,
+        "minutes": 1,
+        "name": "\"../static\"",
+        "percent": 0.09,
+        "text": "1 min",
+        "total_seconds": 63.116156
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "\"game.js\"",
+        "percent": 0.08,
+        "text": "0 secs",
+        "total_seconds": 57.194
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "Flask_SocketIO",
+        "percent": 0.07,
+        "text": "0 secs",
+        "total_seconds": 45.986
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "charset_normalizer",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "warnings",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "logging",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "urllib3",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "cryptography",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "ssl",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "chardet",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 5.469424
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "datetime",
+        "percent": 0.01,
+        "text": "0 secs",
+        "total_seconds": 4.247
+      },
+      {
+        "decimal": "0.00",
+        "digital": "0:00",
+        "hours": 0,
+        "minutes": 0,
+        "name": "\"https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.1/socket.io.min.js\"",
+        "percent": 0.0,
+        "text": "0 secs",
+        "total_seconds": 3.295
+      }
+    ],
+    "editors": [
+      {
+        "decimal": "44.05",
+        "digital": "44:03",
+        "hours": 44,
+        "minutes": 3,
+        "name": "DataSpell",
+        "percent": 97.03,
+        "text": "44 hrs 3 mins",
+        "total_seconds": 158612.36845
+      },
+      {
+        "decimal": "1.33",
+        "digital": "1:20",
+        "hours": 1,
+        "minutes": 20,
+        "name": "VS Code",
+        "percent": 2.97,
+        "text": "1 hr 20 mins",
+        "total_seconds": 4848.757363
+      }
+    ],
+    "end": "2023-06-27T02:59:59Z",
+    "holidays": 0,
+    "human_readable_daily_average": "6 hrs 29 mins",
+    "human_readable_daily_average_including_other_language": "6 hrs 29 mins",
+    "human_readable_range": "last week",
+    "human_readable_total": "45 hrs 24 mins",
+    "human_readable_total_including_other_language": "45 hrs 24 mins",
+    "id": "7bf50584-8dc4-4111-8e55-670067f8ebdb"
+  }
+}
